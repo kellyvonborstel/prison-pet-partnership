0f1aea42211abb42849b21d5860eb351c4b4d75d
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-06-26 15:17:22 -0700

    refactor image handlers

--- public_html/src/main.js
+++ public_html/src/main.js
@@ -1,10 +1,8 @@
-
 $(function() {
 
-  addListeners();
+  addImageHandlers();
 
   // change order of sidebar items when window is resized
-
   $(window).resize(function () {
     var windowWidth = $(window).width();
 
@@ -17,222 +15,55 @@ $(function() {
     }
   }).trigger("resize");
 
-
   // slide menu for small screens
-
-  $('.nav-menu-open').on('click', function(e) {
-    e.preventDefault();
+  $('.nav-menu-open').on('click', function(event) {
+    event.preventDefault();
     $('body').addClass('menu-open');
     $('.nav-menu').addClass('active');
     $('.mask').addClass('active');
     $('.nav-menu-open').disabled = true;
   });
 
-  $('.nav-menu-close').on('click', function(e) {
-    e.preventDefault();
+  $('.nav-menu-close').on('click', function(event) {
+    event.preventDefault();
     $('body').removeClass('menu-open');
     $('.nav-menu').removeClass('active');
     $('.mask').removeClass('active');
     $('.nav-menu-open').disabled = false;
   });
 
-  // added the click handlers to prevent images from disappearing
-  // on ios; '-home' included because path is different for index.html
-
-  function addListeners() {
-
-    $('.js-donate-home').on({
+  // added the click handler to prevent images from disappearing on ios
+  function imageHandler(className, enterImgPath, leaveImgPath) {
+    $(className).on({
       mouseenter: function() {
-        $('.js-donate-home').attr('src', 'images/donate-blue-dark.svg');
+        $(className).attr('src', enterImgPath);
       },
       mouseleave: function() {
-        $('.js-donate-home').attr('src', 'images/donate-blue.svg');
+        $(className).attr('src', leaveImgPath);
       },
       click: function() {
-        $('.js-donate-home').attr('src', 'images/donate-blue.svg');
+        $(className).attr('src', leaveImgPath);
       }
     });
-
-    $('.js-donate').on({
-      mouseenter: function() {
-        $('.js-donate').attr('src', '../images/donate-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-donate').attr('src', '../images/donate-blue.svg');
-      },
-      click: function() {
-        $('.js-donate').attr('src', '../images/donate-blue.svg');
-      }
-    });
-
-    $('.js-boarding-home').on({
-      mouseenter: function() {
-        $('.js-boarding-home').attr('src', 'images/boarding-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-boarding-home').attr('src', 'images/boarding-blue.svg');
-      },
-      click: function() {
-        $('.js-boarding-home').attr('src', 'images/boarding-blue.svg');
-      }
-    });
-
-    $('.js-boarding').on({
-      mouseenter: function() {
-        $('.js-boarding').attr('src', '../images/boarding-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-boarding').attr('src', '../images/boarding-blue.svg');
-      },
-      click: function() {
-        $('.js-boarding').attr('src', '../images/boarding-blue.svg');
-      }
-    });
-
-    $('.js-calendar-home').on({
-      mouseenter: function() {
-        $('.js-calendar-home').attr('src', 'images/calendar-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-calendar-home').attr('src', 'images/calendar-blue.svg');
-      },
-      click: function() {
-        $('.js-calendar-home').attr('src', 'images/calendar-blue.svg');
-      }
-    });
-
-    $('.js-calendar').on({
-      mouseenter: function() {
-        $('.js-calendar').attr('src', '../images/calendar-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-calendar').attr('src', '../images/calendar-blue.svg');
-      },
-      click: function() {
-        $('.js-calendar').attr('src', '../images/calendar-blue.svg');
-      }
-    });
-
-    $('.js-facebook-home').on({
-      mouseenter: function() {
-        $('.js-facebook-home').attr('src', 'images/facebook-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-facebook-home').attr('src', 'images/facebook-blue.svg');
-      },
-      click: function() {
-        $('.js-facebook-home').attr('src', 'images/facebook-blue.svg');
-      }
-    });
-
-    $('.js-facebook').on({
-      mouseenter: function() {
-        $('.js-facebook').attr('src', '../images/facebook-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-facebook').attr('src', '../images/facebook-blue.svg');
-      },
-      click: function() {
-        $('.js-facebook').attr('src', '../images/facebook-blue.svg');
-      }
-    });
-
-    $('.js-twitter-home').on({
-      mouseenter: function() {
-        $('.js-twitter-home').attr('src', 'images/twitter-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-twitter-home').attr('src', 'images/twitter-blue.svg');
-      },
-      click: function() {
-        $('.js-twitter-home').attr('src', 'images/twitter-blue.svg');
-      }
-    });
-
-    $('.js-twitter').on({
-      mouseenter: function() {
-        $('.js-twitter').attr('src', '../images/twitter-blue-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-twitter').attr('src', '../images/twitter-blue.svg');
-      },
-      click: function() {
-        $('.js-twitter').attr('src', '../images/twitter-blue.svg');
-      }
-    });
-
-    $('.js-facebook-footer-home').on({
-      mouseenter: function() {
-        $('.js-facebook-footer-home').attr('src', 'images/facebook-red.svg');
-      },
-      mouseleave: function() {
-        $('.js-facebook-footer-home').attr('src', 'images/facebook-white.svg');
-      },
-      click: function() {
-        $('.js-facebook-footer-home').attr('src', 'images/facebook-white.svg');
-      }
-    });
-
-    $('.js-facebook-footer').on({
-      mouseenter: function() {
-        $('.js-facebook-footer').attr('src', '../images/facebook-red.svg');
-      },
-      mouseleave: function() {
-        $('.js-facebook-footer').attr('src', '../images/facebook-white.svg');
-      },
-      click: function() {
-        $('.js-facebook-footer').attr('src', '../images/facebook-white.svg');
-      }
-    });
-
-    $('.js-linkedin-home').on({
-      mouseenter: function() {
-        $('.js-linkedin-home').attr('src', 'images/linkedin-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-linkedin-home').attr('src', 'images/linkedin.svg');
-      },
-      click: function() {
-        $('.js-linkedin-home').attr('src', 'images/linkedin.svg');
-      }
-    });
-
-    $('.js-linkedin').on({
-      mouseenter: function() {
-        $('.js-linkedin').attr('src', '../images/linkedin-dark.svg');
-      },
-      mouseleave: function() {
-        $('.js-linkedin').attr('src', '../images/linkedin.svg');
-      },
-      click: function() {
-        $('.js-linkedin').attr('src', '../images/linkedin.svg');
-      }
-    });
-
-    $('.js-twitter-footer-home').on({
-      mouseenter: function() {
-        $('.js-twitter-footer-home').attr('src', 'images/twitter-red.svg');
-      },
-      mouseleave: function() {
-        $('.js-twitter-footer-home').attr('src', 'images/twitter-white.svg');
-      },
-      click: function() {
-        $('.js-twitter-footer-home').attr('src', 'images/twitter-white.svg');
-      }
-    });
-
-    $('.js-twitter-footer').on({
-      mouseenter: function() {
-        $('.js-twitter-footer').attr('src', '../images/twitter-red.svg');
-      },
-      mouseleave: function() {
-        $('.js-twitter-footer').attr('src', '../images/twitter-white.svg');
-      },
-      click: function() {
-        $('.js-twitter-footer').attr('src', '../images/twitter-white.svg');
-      }
-    });
-
   }
 
+  function addImageHandlers() {
+    // class names with '-home' included because path is different for images on index.html
+    imageHandler('.js-donate-home', 'images/donate-blue-dark.svg', 'images/donate-blue.svg');
+    imageHandler('.js-donate', '../images/donate-blue-dark.svg', '../images/donate-blue.svg');
+    imageHandler('.js-boarding-home', 'images/boarding-blue-dark.svg', 'images/boarding-blue.svg');
+    imageHandler('.js-boarding', '../images/boarding-blue-dark.svg', '../images/boarding-blue.svg');
+    imageHandler('.js-calendar-home', 'images/calendar-blue-dark.svg', 'images/calendar-blue.svg');
+    imageHandler('.js-calendar', '../images/calendar-blue-dark.svg', '../images/calendar-blue.svg');
+    imageHandler('.js-facebook-home', 'images/facebook-blue-dark.svg', 'images/facebook-blue.svg');
+    imageHandler('.js-facebook', '../images/facebook-blue-dark.svg', '../images/facebook-blue.svg');
+    imageHandler('.js-twitter-home', 'images/twitter-blue-dark.svg', 'images/twitter-blue.svg');
+    imageHandler('.js-twitter', '../images/twitter-blue-dark.svg', '../images/twitter-blue.svg');
+    imageHandler('.js-facebook-footer-home', 'images/facebook-red.svg', 'images/facebook-white.svg');
+    imageHandler('.js-facebook-footer', '../images/facebook-red.svg', '../images/facebook-white.svg');
+    imageHandler('.js-twitter-footer-home', 'images/twitter-red.svg', 'images/twitter-white.svg');
+    imageHandler('.js-twitter-footer', '../images/twitter-red.svg', '../images/twitter-white.svg');
+    imageHandler('.js-linkedin-home', 'images/linkedin-dark.svg', 'images/linkedIn.svg');
+    imageHandler('.js-linkedin', '../images/linkedin-dark.svg', '../images/linkedIn.svg');
+  }
 });
